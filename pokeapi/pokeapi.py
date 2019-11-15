aac688faacffb4b443b2ae2934d302d50df5c96a
Joe Beagle <joe.beagle@kp.org>
2019-11-15 12:16:01 -0500

    working with pandas

---
+++ pokeapi/pokeapi.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+
+#import Requests
+
+# Define the URL we want to use
+POKEURL = "http://pokeapi.co/api/v2/pokemon/"
+
+def main():
+    # use requests libary to send an HTTP GET
+    
+    
+    #Strip of json
+    
+    
+    #Print out all of the names of pokemon returned in the request!
+    
+    
+if __name__ == "__main__":
+    main()
